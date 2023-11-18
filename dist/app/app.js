a2b44028915194ac767aa875ff18e55a80cfe763
JubaStriker <jubastriker@gmail.com>
2023-11-18 12:08:21 +0600

    Router covered

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,7 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
+app.use(express_1.default.json());
+const useRouter = express_1.default.Router();
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.use('/api/v1', useRouter);
+useRouter.get('/user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: 'User successfully created',
+        data: user
+    });
+});
+app.get('/', logger, (req, res) => {
     res.send('Hello Developers!');
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,31 @@
-import express from 'express';
-const app = express()
+import express, { NextFunction, Request, Response } from 'express';
 
+const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+const useRouter = express.Router();
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.url, req.method, req.hostname)
+    next();
+}
+
+app.use('/api/v1', useRouter);
+
+useRouter.get('/user', (req: Request, res: Response) => {
+    const user = req.body;
+    console.log(user);
+
+    res.json({
+        success: true,
+        message: 'User successfully created',
+        data: user
+    })
+})
+
+app.get('/', logger, (req: Request, res: Response) => {
+
     res.send('Hello Developers!')
 })
 
